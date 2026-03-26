84f524bd12cc012598353d76f86a27543837553b
Carl Nelson <onelastrhythm@gmail.com>
2026-03-26 15:06:55 -0400

    feat: Update Kotlin, KSP, and AGP versions, add new reader and settings feature modules, and refactor the Injekt API.

---
+++ feature/reader/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("mihon.library")
+    id("mihon.library.compose")
+    kotlin("android")
+}
+
+android {
+    namespace = "ephyra.feature.reader"
+    
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(projects.core.common)
+    implementation(projects.domain)
+    implementation(projects.data)
+    implementation(projects.sourceApi)
+    implementation(projects.sourceLocal)
+    implementation(projects.i18n)
+    implementation(projects.presentationCore)
+
+    implementation(libs.logcat)
+    implementation(libs.bundles.voyager)
+    implementation(libs.koin.android)
+    
+    implementation(libs.subsamplingscaleimageview)
+    implementation(libs.image.decoder)
+}
