a7f293353ff4dc746c453a4606d676ccfebb7ff2
Gameaday <onelastrhythm@gmail.com>
2026-03-28 13:48:41 -0400

    feat: initialize settings feature module and update dependency catalog

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -15,6 +15,12 @@ dependencies {
     implementation(projects.i18n)
     implementation(projects.presentationCore)
 
+    implementation(compose.material3.core)
+    implementation(compose.material.icons)
+    implementation(compose.foundation)
+    implementation(compose.ui.tooling.preview)
+    debugImplementation(compose.ui.tooling)
+
     implementation(libs.bundles.voyager)
     implementation(libs.koin.android)
 }
