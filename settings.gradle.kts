459432bf65297fa77d6f7e6b77a64a1b619be816
Gameaday <onelastrhythm@gmail.com>
2026-03-28 16:42:33 -0400

    chore: remove buildSrc module from project settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,7 +41,6 @@ dependencyResolutionManagement {
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "Ephyra"
-include(":buildSrc")
 include(":app")
 include(":core-metadata")
 include(":core:archive")
