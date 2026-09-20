f8a9d9c786be75bdf29db42f68c3b418a4e69954
Gameaday <onelastrhythm@gmail.com>
2026-09-20 15:54:48 -0400

    update dependencies

--- buildSrc/src/main/kotlin/ephyra/buildlogic/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/ephyra/buildlogic/AndroidConfig.kt
@@ -6,8 +6,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget as KotlinJvmTarget
 object AndroidConfig {
     // 37 required by Coil 3.6.x AAR metadata (compileSdk can lead targetSdk).
     const val COMPILE_SDK = 37
-    const val TARGET_SDK = 36
-    const val MIN_SDK = 34
+    const val TARGET_SDK = 37
+    const val MIN_SDK = 36
 
     // https://youtrack.jetbrains.com/issue/KT-66995/JvmTarget-and-JavaVersion-compatibility-for-easier-JVM-version-setup
     val JavaVersion = GradleJavaVersion.VERSION_17
